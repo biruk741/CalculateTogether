a22edc9d09c4c67baa419a7d0502d63a7124667c
Biruk <mengi024@morris.umn.edu>
2021-03-11 23:29:35 -0600

    started working on mobile design, set up firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBsUBRZPb4iP58Wjx-_Uyo-FJzIVikezcg",
+    authDomain: "calculatetogether.firebaseapp.com",
+    databaseURL: "https://calculatetogether-default-rtdb.firebaseio.com",
+    projectId: "calculatetogether",
+    storageBucket: "calculatetogether.appspot.com",
+    messagingSenderId: "325928134961",
+    appId: "1:325928134961:web:8ac30bcf2164d58f8c2810"
+  }
 };
